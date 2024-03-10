d06ad1f8a6b93c38702ae538414b6038d14808f6
paulog22 <paulog2204@gmail.com>
2024-03-10 03:14:45 -0300

    Added UserEmail and Password class

---
+++ src/main/java/com/paulo/minipicpay/model/Password.java
@@ -0,0 +1,10 @@
+package com.paulo.minipicpay.model;
+
+public class Password {
+
+    private final String value;
+
+    public Password(String value) {
+        this.value = value;
+    }
+}
---
+++ src/main/java/com/paulo/minipicpay/model/UserEmail.java
@@ -0,0 +1,10 @@
+package com.paulo.minipicpay.model;
+
+public class UserEmail {
+
+    private final String value;
+
+    public UserEmail(String value) {
+        this.value = value;
+    }
+}

0c5df05ad6562a697ab84065779391d7ffbf0901
paulog22 <paulog2204@gmail.com>
2024-03-10 03:46:15 -0300

    Refactored Password class

--- src/main/java/com/paulo/minipicpay/model/Password.java
+++ src/main/java/com/paulo/minipicpay/model/Password.java
@@ -4,7 +4,15 @@ public class Password {
 
     private final String value;
 
-    public Password(String value) {
+    private Password(String value) {
         this.value = value;
+    }
+
+    public static Password of(String value) {
+        return new Password(value);
+    }
+
+    public String value() {
+        return value;
     }
 }
